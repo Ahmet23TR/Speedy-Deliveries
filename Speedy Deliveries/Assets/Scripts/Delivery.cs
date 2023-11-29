58c63e86bcac440998bc91bc69318379d668e407
Ahmet Emin <gucluahmetemin@gmail.com>
2023-11-29 22:15:23 +0300

    Speedy-Deliveries 1.0

--- Speedy Deliveries/Assets/Scripts/Delivery.cs
+++ Speedy Deliveries/Assets/Scripts/Delivery.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    
-    void Update() 
+    SpriteRenderer spriteRenderer;
+    [SerializeField] Color secondColor = new Color(255,0,0); 
+    [SerializeField] Color firstColor = new Color(255,255,255); 
+    void Start() 
     {
-        
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();    
     }
-    void OnCollisionEnter2D(Collision2D other)
+    [SerializeField] float timeToDestroy = 0.5f;
+    bool hasPackage;
+
+     void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("You bumped");    
+        if(other.CompareTag("Package") && !hasPackage)
+        {
+            Debug.Log("You just take the package!");        
+            Destroy(other.gameObject, timeToDestroy);  
+            hasPackage = true;
+            spriteRenderer.color = secondColor;
+        }
+        else if(hasPackage && other.CompareTag("Customer"))
+        {
+            Debug.Log("Well done you delivered your package!");   
+            hasPackage = false;     
+            Destroy(other.gameObject, timeToDestroy);  
+            spriteRenderer.color = firstColor;
+        }
     }
-    
 }
--- Speedy Deliveries/Assets/Scripts/Driver.cs
+++ Speedy Deliveries/Assets/Scripts/Driver.cs
@@ -6,6 +6,9 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.1f;
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float slowSpeed = 20f;
+    [SerializeField] float boostSpeed = 40f;
+    [SerializeField] float speedDuration = 10f;
 
     void Start()
     {
@@ -18,5 +21,22 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+
     }
+        void OnTriggerEnter2D(Collider2D other) 
+        {
+            if(other.CompareTag("Booster") && speedDuration > 0)
+            {
+                moveSpeed = boostSpeed;
+                speedDuration -= Time.deltaTime;
+                Destroy(other.gameObject);
+            }
+            else if(other.CompareTag("Bumper") && speedDuration > 0)
+            {
+                moveSpeed = slowSpeed;
+                speedDuration -= Time.deltaTime;
+                Destroy(other.gameObject);
+            }
+       
+        }
 }
